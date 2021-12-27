405330a5cb583852570bb0c4d89ce6c7decf28dd
Gisela Calva <gisela.calva.241@my.csun.edu>
2021-12-26 19:42:57 -0800

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+/*Gisela Calva
+11.18.21
+This program executes the phrase Hello World. Hello World is said to have been
+first used as a test phrase by Brian Kernighams 1972 book, an introduction to 
+the language B. C has currently being developed at the time. */
+
+//HelloWorld is the class name
+public class HelloWorld{
+    
+    //this is the main method, the entry point where the program begins to execute
+    //the main method contains the word String which means its a sequence of characters
+    //
+    public static void main(String[] args) {
+    // Displays the message on the console
+    System.out.println("Hello World");
+    }
+}
